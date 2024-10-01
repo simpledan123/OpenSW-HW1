f7a98cf6c01396308eb3be8ccf1eb79c57a8b35a
simpledan123 <yundan928@gmail.com>
2024-10-01 10:34:35 +0900

    SECOND COMMIT

--- caculate.py
+++ caculate.py
@@ -1,11 +1,15 @@
 x = int(input("첫 번쨰 숫자 입력:"))
 y = int(input("두 번째 숫자 입력:"))
-chose = input("원하는 계산 방식 입력 [+, - 중 선택]")
+chose = input("원하는 계산 방식 입력 [+, - , *중 선택]")
 
 if chose == "+":
     print(x+y)
 
 elif chose == "-":
     print(x-y)
+
+elif chost == "*":
+    print(x*y)
+    
 else : 
     print("error")
